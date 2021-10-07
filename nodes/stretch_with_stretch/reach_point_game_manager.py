3c8397c8c03d79e41800a0c6434e89cf3ed9ecab
team-blue-stretch <31256490+mlamsey@users.noreply.github.com>
2021-10-07 10:53:55 -0700

    adds game manager and improved touch detection

---
+++ nodes/stretch_with_stretch/reach_point_game_manager.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+# Generic Imports
+from __future__ import print_function
+import ros
+
+# ROS
+import rospy
+
+# Messages
+from std_msgs.msg import Bool
+
+class reach_point_game_manager:
+    def __init__(self):
+        rospy.init_node('reach_point_game_manager', anonymous=True)
+        self.rate = 10
+
+        self.wrist_contact = False
+        self.wrist_contact_subscriber = rospy.Subscriber('/wrist_contact_detected', Bool, self.wrist_contact_callback)
+
+        self.point_scored_publisher = rospy.Publisher('/game_state/point_scored', Bool, queue_size=1)
+        self.start_game_publisher = rospy.Publisher('/game_state/start_game', Bool, queue_size=1)
+
+        self.game_score = 0
+
+    def wrist_contact_callback(self, data):
+        self.wrist_contact = data.data
+
+    def point_scored_action(self):
+        rospy.loginfo("Point Scored!")
+        self.game_score += 1
+        self.point_scored_publisher.publish(True)
+        rospy.sleep(0.25)
+
+    def main(self):
+        rate = rospy.Rate(self.rate)
+
+        rospy.sleep(3)
+        self.start_game_publisher.publish(True)
+
+        # Game state
+        score_changed = False
+
+        while not rospy.is_shutdown():
+            if self.wrist_contact:
+                self.point_scored_action()
+                score_changed = True
+
+            if score_changed:
+                rospy.loginfo(self.game_score)
+                score_changed = False
+
+            rate.sleep()
+
+if __name__ == '__main__':
+    node = reach_point_game_manager()
+    node.main()
--- nodes/stretch_with_stretch/stretch_sound.py
+++ nodes/stretch_with_stretch/stretch_sound.py
@@ -19,7 +19,8 @@ class StretchSound:
     def __init__(self):
         rospy.init_node("stretch_sound", anonymous=True)
 
-        self.wrist_contact_subscriber = rospy.Subscriber('/wrist_contact_detected', Bool, self.wrist_contact_callback, queue_size=1)
+        self.point_scored_subscriber = rospy.Subscriber('/game_state/point_scored', Bool, self.point_scored, queue_size=1)
+        self.start_game_subscriber = rospy.Subscriber('/game_state/start_game', Bool, self.start_exercise, queue_size=1)
 
         self.base_sound_path = "/home/hello-robot/catkin_ws/src/stretch-caregiving-class/sounds/"
 
@@ -27,36 +28,31 @@ class StretchSound:
         self.rate = 20
         self.handle = SoundClient()
 
-    def wrist_contact_callback(self, data):
-        rospy.loginfo(data.data)
-        if data.data:
-            self.point_scored()
-
     def main(self):
-        rate = rospy.Rate(self.rate)
+        rospy.spin()
+        # rate = rospy.Rate(self.rate)
 
-        rospy.sleep(1)
-        self.start_exercise()
+        # rospy.sleep(1)
+        # self.start_exercise()
 
-        while not rospy.is_shutdown():
-            # self.point_scored()
-            # play sound
-            # msg = "hi everyone"
-            # rospy.loginfo("say {}".format(msg))
-            # self.handle.say(msg)
+        # while not rospy.is_shutdown():
+        #     # self.point_scored()
+        #     # play sound
+        #     # msg = "hi everyone"
+        #     # rospy.loginfo("say {}".format(msg))
+        #     # self.handle.say(msg)
 
-            rate.sleep()
+        #     rate.sleep()
 
-    def start_exercise(self):
-        start_exercise = self.base_sound_path + "3-2-1-go_75bpm.wav"
-        self.handle.playWave(start_exercise,blocking=True)
-        rospy.sleep(2)
+    def start_exercise(self, data):
+        if data.data:
+            start_exercise = self.base_sound_path + "3-2-1-go_75bpm.wav"
+            self.handle.playWave(start_exercise,blocking=True)
 
-    def point_scored(self):
-        rospy.loginfo("Point Scored!")
-        point_scored = self.base_sound_path + "point_scored.wav"
-        self.handle.playWave(point_scored,blocking=False)
-        # rospy.sleep(0.1)
+    def point_scored(self, data):
+        if data.data:
+            point_scored = self.base_sound_path + "point_scored.wav"
+            self.handle.playWave(point_scored,blocking=False)
 
 if __name__ == "__main__":
     node = StretchSound()
